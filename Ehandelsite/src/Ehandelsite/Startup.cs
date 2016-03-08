7f9da4df4ba70b7738bd690bc824d61a533d88ef
Billy Askefors <b_askefors@hotmail.com>
2016-03-08 15:09:14 +0100

    StartUp now with app.use

--- Ehandelsite/src/Ehandelsite/Startup.cs
+++ Ehandelsite/src/Ehandelsite/Startup.cs
@@ -21,12 +21,9 @@ namespace Ehandelsite
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
