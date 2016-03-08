3390d1b6f6d57e2310e5cc199816aedd74a2c2b4
Billy Askefors <b_askefors@hotmail.com>
2016-03-08 15:44:33 +0100

    Random changes

--- Ehandelsite/src/Ehandelsite/ViewModels/Customer.cs
+++ Ehandelsite/src/Ehandelsite/ViewModels/Customer.cs
@@ -7,11 +7,11 @@ namespace Ehandelsite.ViewModels
 {
     public class Customer
     {
-        public Customer(string email, string pword, string ssn, int userType) : base(email, pword, ssn, userType)
-        {
-            OrderHistory = new List<Order>();
-        }
-        public int CompanyID { get; set; }
-        public List<Order> OrderHistory { get; set; }
+        //public Customer(string email, string pword, string ssn, int userType) : base(email, pword, ssn, userType)
+        //{
+        //    OrderHistory = new List<Order>();
+        //}
+        //public int CompanyID { get; set; }
+        //public List<Order> OrderHistory { get; set; }
     }
 }
